9c7026c8899174fcce1468bccb670591b522384f
Kévin Menant <menant.kevin@hotmail.fr>
2015-12-18 17:57:21 +0100

    Ajout de classe Java

--- Android/StarsUp/app/src/main/java/com/example/ppe/starsup/Connexion_Activity.java
+++ Android/StarsUp/app/src/main/java/com/example/ppe/starsup/Connexion_Activity.java
@@ -1,18 +1,53 @@
 package com.example.ppe.starsup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 
 public class Connexion_Activity extends Activity {
+
+    private Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        Button btnL = (Button) findViewById(R.id.btn_login);
+        btnL.setOnClickListener(click);
     }
+
+    private View.OnClickListener click = new View.OnClickListener() {
+        //redéfinition de la méthode onClick
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_login : //gestion du click du bouton
+                    Toast.makeText(getApplicationContext(), "Connexion ...", Toast.LENGTH_SHORT).show();
+
+                        String email = ((EditText)findViewById(R.id.txt_Email)).getText().toString();
+                        String mdp = ((EditText)findViewById(R.id.txt_MDP)).getText().toString();
+
+                        //if login est bon
+                            i = new Intent(getApplicationContext(), Planning_Activity.class);
+                            startActivity(i);
+                            Toast.makeText(getApplicationContext(),"Valide",Toast.LENGTH_SHORT).show();
+
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ Android/StarsUp/app/src/main/java/com/example/ppe/starsup/HEBERGEMENT.java
@@ -0,0 +1,79 @@
+package com.example.ppe.starsup;
+
+/**
+ * Created by menant on 18/12/2015.
+ */
+public class HEBERGEMENT {
+    private Integer ID_DEPARTEMENT;
+    private Integer ID_HEBERGEMENT ;
+    private String NOM_HEBERGEMENT;
+    private String ADRESSE_HEBERGEMENT;
+    private String VILLE_HEBERGEMENT;
+
+    //region accesseur
+    public Integer getID_DEPARTEMENT() {
+        return ID_DEPARTEMENT;
+    }
+
+    public void setID_DEPARTEMENT(Integer ID_DEPARTEMENT) {
+        this.ID_DEPARTEMENT = ID_DEPARTEMENT;
+    }
+
+    public Integer getID_HEBERGEMENT() {
+        return ID_HEBERGEMENT;
+    }
+
+    public void setID_HEBERGEMENT(Integer ID_HEBERGEMENT) {
+        this.ID_HEBERGEMENT = ID_HEBERGEMENT;
+    }
+
+    public String getNOM_HEBERGEMENT() {
+        return NOM_HEBERGEMENT;
+    }
+
+    public void setNOM_HEBERGEMENT(String NOM_HEBERGEMENT) {
+        this.NOM_HEBERGEMENT = NOM_HEBERGEMENT;
+    }
+
+    public String getADRESSE_HEBERGEMENT() {
+        return ADRESSE_HEBERGEMENT;
+    }
+
+    public void setADRESSE_HEBERGEMENT(String ADRESSE_HEBERGEMENT) {
+        this.ADRESSE_HEBERGEMENT = ADRESSE_HEBERGEMENT;
+    }
+
+    public String getVILLE_HEBERGEMENT() {
+        return VILLE_HEBERGEMENT;
+    }
+
+    public void setVILLE_HEBERGEMENT(String VILLE_HEBERGEMENT) {
+        this.VILLE_HEBERGEMENT = VILLE_HEBERGEMENT;
+    }
+    //endregion
+
+    //region CONSTRUCTEUR
+    public HEBERGEMENT(Integer unDep, String unN, String uneA, String uneV){
+        ID_DEPARTEMENT = unDep;
+        NOM_HEBERGEMENT = unN;
+        ADRESSE_HEBERGEMENT = uneA;
+        VILLE_HEBERGEMENT= uneV ;
+
+    }
+
+    public HEBERGEMENT(){
+
+    }
+    //endregion
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\t departement : ").append(ID_DEPARTEMENT);
+        sb.append(" ville : ").append(VILLE_HEBERGEMENT).append("\n");
+        sb.append(" adresse : ").append(ADRESSE_HEBERGEMENT ).append("\n");
+        sb.append(" nom : ").append(NOM_HEBERGEMENT ).append("\n");
+        return sb.toString();
+    }
+
+
+}
