798357b72ccdb556572963495f1ed69f9b6e6629
Natacha Bouhours <natachabou49@gmail.com>
2016-03-08 15:53:52 +0100

    btnRediger
    ecoute du boutton rediger

--- Android/StarsUp/app/src/main/java/com/example/ppe/starsup/Hebergement_Activity.java
+++ Android/StarsUp/app/src/main/java/com/example/ppe/starsup/Hebergement_Activity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Hebergement_Activity extends Activity {
@@ -12,7 +14,17 @@ public class Hebergement_Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hebergement);
+
+        Button Btn_rediger=(Button) findViewById(R.id.btn_rediger);
+        Btn_rediger.setOnClickListener(btnClickRedig); // btnclick à garder
     }
+
+
+    private View.OnClickListener btnClickRedig = new View.OnClickListener() {
+        public void onClick(View v) {
+        }
+
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
